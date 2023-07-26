4f3de5104bc99ed2a206c643b3000c354c9be095
gangjoohyeong <bles@kakao.com>
2023-07-26 02:05:42 +0000

    fix: model load

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from starlette.middleware.cors import CORSMiddleware
 import uvicorn
 from polyglot import model_settings, gen
@@ -15,13 +15,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-model, tokenizer = model_settings()
 
+# 모델과 토크나이저를 로드하는 의존성(dependency) 함수를 정의합니다.
+def get_model_tokenizer():
+    model, tokenizer = model_settings()
+    return model, tokenizer
+
+# 의존성을 라우트 함수에 사용합니다.
 @app.post("/get_prediction/")
-async def get_prediction(input: str):
+async def get_prediction(input: str, model_tokenizer=Depends(get_model_tokenizer)):
+    model, tokenizer = model_tokenizer
     output = gen(model, tokenizer, input)
     return {'output': output}
 
-
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=30013, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=40001, reload=True)
